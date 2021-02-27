7843e9e3421304dd359460630b60a1f173f51af0
Jos Sloof <A.J.G.sloof@student.tudelft.nl>
2021-02-27 17:07:31 +0100

    Add QuestionRepository to repositories

---
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/QuestionRepository.java
@@ -0,0 +1,8 @@
+package nl.tudelft.oopp.demo.repositories;
+
+import nl.tudelft.oopp.demo.entities.Question;
+import nl.tudelft.oopp.demo.entities.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
