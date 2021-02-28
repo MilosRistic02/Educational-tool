7caad3b329268bbf6f1f6e286a5cfe29285f54be
Marc Neeleman <m.r.w.neeleman@student.tudelft.nl>
2021-02-28 10:24:48 +0100

    Added the LectureRoom entity and wrote tests. The pin is not generated yet.

---
+++ server/src/test/java/nl/tudelft/oopp/demo/entities/LectureRoomTest.java
@@ -0,0 +1,59 @@
+package nl.tudelft.oopp.demo.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LectureRoomTest {
+
+    LectureRoom lectureRoom;
+
+    @BeforeEach
+    public void setup(){
+        lectureRoom = new LectureRoom(1, 2);
+    }
+
+    @Test
+    void getLecturerID() {
+        assertEquals(lectureRoom.getLecturerID(), 1);
+    }
+
+    @Test
+    void setLecturerID() {
+        lectureRoom.setLecturerID(2);
+        assertEquals(lectureRoom.getLecturerID(), 2);
+    }
+
+    @Test
+    void getCourseId() {
+        assertEquals(lectureRoom.getCourseId(), 2);
+    }
+
+    @Test
+    void setCourseId() {
+        lectureRoom.setCourseId(5);
+        assertEquals(lectureRoom.getCourseId(), 5);
+    }
+
+//    @Test
+//    void getLecturePin() {
+//        //TODO
+//    }
+
+//    @Test
+//    void setLecturePin() {
+//        //TODO
+//    }
+
+//    @Test
+//    void getTimestamp() {
+//
+//    }
+
+//    @Test
+//    void testEquals() {
+//        LectureRoom lectureRoom1 = new LectureRoom(2, 3);
+    //TODO
+//    }
+}
