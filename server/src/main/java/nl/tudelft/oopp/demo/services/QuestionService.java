4af19cff1448e017639c9226623445c78df828b7
Jos Sloof <A.J.G.sloof@student.tudelft.nl>
2021-03-01 11:56:51 +0100

    Add endpoints for adding, retrieving and deleting questions to/from the database

--- server/src/main/java/nl/tudelft/oopp/demo/services/QuestionService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/QuestionService.java
@@ -5,6 +5,8 @@ import nl.tudelft.oopp.demo.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class QuestionService {
 
@@ -27,5 +29,38 @@ public class QuestionService {
         prev.setScore(question.getScore());
         questionRepository.save(prev);
         return "Updated score of Question";
+    }
+
+
+    /**
+     * Gets all questions.
+     *
+     * @return all questions found in the database
+     */
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    /**
+     * Adds a question to the database
+     *
+     * @param question question to add to the database
+     */
+    public void addQuestion(Question question) {
+        questionRepository.save(question);
+    }
+
+    /**
+     * Remove a question from the database iff the question exists
+     *
+     * @param id the id that uniquely identifies each question
+     * @return boolean that is true iff a question was deleted
+     */
+    public boolean deleteQuestion(long id) {
+        if(!questionRepository.existsById(id))
+            return false;
+
+        questionRepository.deleteById(id);
+        return true;
     }
 }
