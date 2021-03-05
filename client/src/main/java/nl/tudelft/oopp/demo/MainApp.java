bfd8e2c6cde1347b28098928a7ace57d63dd16de
Milos Ristic <m.ristic-1@student.tudelft.nl>
2021-03-05 10:02:48 +0100

    Cleaned up LogInController

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,9 +1,10 @@
 package nl.tudelft.oopp.demo;
 
+import nl.tudelft.oopp.demo.views.LogInDisplay;
 import nl.tudelft.oopp.demo.views.QuoteDisplay;
 
 public class MainApp {
     public static void main(String[] args) {
-        QuoteDisplay.main(new String[0]);
+        LogInDisplay.main(new String[0]);
     }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LogInController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LogInController.java
@@ -31,25 +31,15 @@ public class LogInController {
         String username = usernameField.getText();
         String password = passwordField.getText();
         reset();
-        if(username == null || password == null || username.length() == 0 || password.length() == 0) {
+        if(username.length() == 0 || password.length() == 0) {
             emptyFields.setVisible(true);
             usernameField.requestFocus();
             return;
         }
 
         String response = ServerCommunication.sendCredentials(username, password);
-        if(response.equals("User doesn't exist")) {
-            invalidUser.setVisible(true);
-            usernameField.setText("");
-            usernameField.requestFocus();
-            return;
-        }
-        if(response.equals("Wrong password")) {
-            wrongPassword.setVisible(true);
-            passwordField.setText("");
-            passwordField.requestFocus();
-            return;
-        }
+        if(!checkValidResponse(response)) return;
+
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("You logged in");
@@ -63,5 +53,19 @@ public class LogInController {
         wrongPassword.setVisible(false);
         invalidUser.setVisible(false);
         passwordField.setText("");
+    }
+
+    private boolean checkValidResponse(String response) {
+        if(response.contains("{")) return true;
+        if(response.equals("User doesn't exist")) {
+            invalidUser.setVisible(true);
+            usernameField.setText("");
+            usernameField.requestFocus();
+            return false;
+        }
+
+        wrongPassword.setVisible(true);
+        passwordField.requestFocus();
+        return false;
     }
 }
