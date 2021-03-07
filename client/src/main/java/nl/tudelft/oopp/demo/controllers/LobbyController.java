2b57b11a66d9d2111a27e36827e8a6e2815bcaeb
Marc Neeleman <m.r.w.neeleman@student.tudelft.nl>
2021-03-07 18:01:15 +0100

    Added javaDoc to the lobbyController and renamed the method that redirects the user to the StudentLobby.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/LobbyController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/LobbyController.java
@@ -20,19 +20,37 @@ public class LobbyController {
     @FXML
     AnchorPane rootPane;
 
+    /**
+     * Creates a new lectureRoom and redirects the lecturer to that room.
+     */
+    @FXML
+    public void createRoom(){
+        // use the create database method
+        // TODO
 
+        // redirect the user to the next scene
+        // TODO
+    }
+
+    /**
+     * Redirects the user to the next scene, where they can enter a lecture pin.
+     *
+     * @throws IOException
+     */
+    @FXML
+    public void joinLobby() throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/lobbyStudent.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    /**
+     * Checks if the lecture pin is currently in the database.
+     * In that case, the player joins the lectureRoom with the corresponding pin,
+     * else, the player is alerted that the pin is not valid, and can try to enter another pin.
+     */
     @FXML
     private void checkPin() {
         String pin = pinText.getText();
-
-        //TODO
-        // lobby voor mod/lecturer
-        // issues gitlab
-
-
-        // Check if the pinText is an existing lecturePin with queries
-        // if it exists, joinRoom(lecturePin)
-        // else give an alert that the pin is not correct
         if (ServerCommunication.checkPin(pin)) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Correct pin");
@@ -46,13 +64,6 @@ public class LobbyController {
             alert.setContentText("This lecture pin is not valid");
             alert.showAndWait();
         }
-
-
-    }
-
-    public void enterPin() throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("/lobbyStudent.fxml"));
-        rootPane.getChildren().setAll(pane);
     }
 
 }
