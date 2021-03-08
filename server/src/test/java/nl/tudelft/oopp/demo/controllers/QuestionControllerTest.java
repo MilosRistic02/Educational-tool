b78ffa8a1829f6be7547fa6ae9c0180f56ef570b
Jos Sloof <A.J.G.sloof@student.tudelft.nl>
2021-03-08 21:49:29 +0100

    add tests for question controller and question service

--- server/src/test/java/nl/tudelft/oopp/demo/controllers/QuestionControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/controllers/QuestionControllerTest.java
@@ -1,24 +1,54 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import nl.tudelft.oopp.demo.entities.Question;
+import nl.tudelft.oopp.demo.repositories.QuestionRepository;
+import nl.tudelft.oopp.demo.services.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class QuestionControllerTest {
+
+    @Autowired
+    private QuestionController questionController;
+
+    @MockBean
+    private QuestionService questionService;
+
+    private Question question1 = new Question("question", "4812421dristic", "me");
+    private Question question2 = new Question("question2", "1889221jsloof", "myself");
 
     @Test
     void changeUpvote() {
+        Mockito.when(questionService.updateScoreQuestion(question1)).thenReturn("Updated score of Question");
+        assertEquals("Updated score of Question", questionController.changeUpvote(question1));
     }
 
     @Test
     void getAllQuestions() {
+        Mockito.when(questionService.getAllQuestions()).thenReturn(Arrays.asList(question1, question2));
+        assertArrayEquals(Arrays.asList(question1, question2).toArray(), questionService.getAllQuestions().toArray());
     }
 
     @Test
     void addQuestion() {
+        Mockito.when(questionService.addQuestion(question1)).thenReturn(question1.getQuestion());
+        assertEquals(question1.getQuestion(), questionController.addQuestion(question1));
     }
 
     @Test
     void deleteQuestion() {
+        Mockito.when(questionService.deleteQuestion(61)).thenReturn(true);
+        assertTrue(questionController.deleteQuestion(61));
     }
 }
--- server/src/test/java/nl/tudelft/oopp/demo/services/QuestionServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/services/QuestionServiceTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Arrays;
+import java.util.Queue;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,6 +25,7 @@ class QuestionServiceTest {
     private QuestionRepository questionRepository;
 
     private Question question1 = new Question("question", "4812421dristic", "me");
+    private Question question2 = new Question("question2", "1889221jsloof", "myself");
 
 
     @Test
@@ -41,21 +45,50 @@ class QuestionServiceTest {
 
     @Test
     void updateQuestionAnsweredStatus() {
+        question1.setAnswered(true);
+        Mockito.when(questionRepository.existsByLecturePin("4812421dristic")).thenReturn(true);
+        Mockito.when(questionRepository.getByLecturePin("4812421dristic")).thenReturn(question1);
+        assertTrue(question1.isAnswered());
+        Mockito.when(questionRepository.save(question1)).thenReturn(question1);
+        assertEquals(questionService.updateQuestionAnsweredStatus(question1)
+                , "The answered status of the question has been updated.");
+    }
+
+    @Test
+    void updateQuestionAnsweredStatusNeg() {
+        Mockito.when(questionRepository.existsByLecturePin("4812421dristic")).thenReturn(false);
+        assertEquals(questionService.updateQuestionAnsweredStatus(question1)
+                , "The question is not in the database.");
     }
 
     @Test
     void isQuestionAnswered() {
+        question1.setAnswered(true);
+        Mockito.when(questionRepository.getByLecturePin("4812421dristic")).thenReturn(question1);
+        assertTrue(questionService.isQuestionAnswered(question1));
     }
 
     @Test
     void getAllQuestions() {
+        Mockito.when(questionRepository.findAll()).thenReturn(Arrays.asList(question1, question2));
+        assertArrayEquals(Arrays.asList(question1, question2).toArray(), questionService.getAllQuestions().toArray());
     }
 
     @Test
     void addQuestion() {
+        Mockito.when(questionRepository.save(question1)).thenReturn(question1);
+        assertEquals(question1.getQuestion(), questionService.addQuestion(question1));
     }
 
     @Test
     void deleteQuestion() {
+        Mockito.when(questionRepository.existsById((long) 61)).thenReturn(true);
+        assertTrue(questionService.deleteQuestion((long) 61));
+    }
+
+    @Test
+    void deleteQuestionNeg() {
+        Mockito.when(questionRepository.existsById((long) 61)).thenReturn(false);
+        assertFalse(questionService.deleteQuestion((long) 61));
     }
 }
