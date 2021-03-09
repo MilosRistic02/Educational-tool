7a8fb78371f8b83aeb75a615fe376b4ed9e735fa
Jos Sloof <A.J.G.sloof@student.tudelft.nl>
2021-03-09 20:43:17 +0100

    fix checkstyle usersservice

--- server/src/main/java/nl/tudelft/oopp/demo/services/UsersService.java
+++ server/src/main/java/nl/tudelft/oopp/demo/services/UsersService.java
@@ -1,11 +1,14 @@
 package nl.tudelft.oopp.demo.services;
 
+import java.util.LinkedList;
+import java.util.List;
 import nl.tudelft.oopp.demo.entities.Users;
 import nl.tudelft.oopp.demo.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+
+
 
 @Service
 public class UsersService {
@@ -14,14 +17,12 @@ public class UsersService {
 
     /**
      * UsersService constructor to utilize dependency injection.
-     * @param usersRepo
+     * @param usersRepo the users repository
      */
     @Autowired
-    public UsersService(UsersRepository usersRepo){
+    public UsersService(UsersRepository usersRepo) {
         this.usersRepo = usersRepo;
-
     }
-
 
     /**
      * Service to get all users.
@@ -39,7 +40,9 @@ public class UsersService {
         List<Users> allUsers = usersRepo.findAll();
         List<Users> allStudents = new LinkedList<>();
         for (Users user : allUsers) {
-            if (user.getRole().equals("student")) allStudents.add(user);
+            if (user.getRole().equals("student")) {
+                allStudents.add(user);
+            }
         }
         return allStudents;
     }
@@ -51,8 +54,10 @@ public class UsersService {
     public List<Users> getAllModerators() {
         List<Users> allUsers = usersRepo.findAll();
         List<Users> allModerators = new LinkedList<>();
-        for(Users user : allUsers) {
-            if (user.getRole().equals("moderator")) allModerators.add(user);
+        for (Users user : allUsers) {
+            if (user.getRole().equals("moderator")) {
+                allModerators.add(user);
+            }
         }
         return allModerators;
     }
@@ -64,82 +69,86 @@ public class UsersService {
     public List<Users> getAllLecturers() {
         List<Users> allUsers = usersRepo.findAll();
         List<Users> allLecturers = new LinkedList<>();
-        for(Users user : allUsers) {
-            if (user.getRole().equals("lecturer")) allLecturers.add(user);
+        for (Users user : allUsers) {
+            if (user.getRole().equals("lecturer")) {
+                allLecturers.add(user);
+            }
         }
         return allLecturers;
     }
 
     /**
      * This methods adds a user to the database.
-     * @param user
+     * @param user the user
      * @return Success/Error message
      */
-    public String addUser(Users user){
-        if(usersRepo.existsByUsername(user.getUsername())){
+    public String addUser(Users user) {
+        if (usersRepo.existsByUsername(user.getUsername())) {
             return "This user already exists!";
         }
         usersRepo.save(user);
-        return "User type: " + user.getRole() +
-                ", username: " + user.getUsername() +
-                " has been added successfully";
+        return "User type: " + user.getRole()
+                + ", username: " + user.getUsername()
+                + " has been added successfully";
     }
 
     /**
      * A method that deletes an already existing user from the database.
-     * @param user
+     * @param user the user
      * @return Error message iff the user does not exists,
-     * Success if the user is successfully deleted from the database.
+     *     Success if the user is successfully deleted from the database.
      */
-    public String deleteUser(Users user){
-        if(!usersRepo.existsByUsername(user.getUsername())){
+    public String deleteUser(Users user) {
+        if (!usersRepo.existsByUsername(user.getUsername())) {
             return "This user does not exist!";
         }
         usersRepo.delete(user);
-        return "User, named: " +
-                user.getUsername() +
-                ", has been deleted from the database";
+        return "User, named: "
+                + user.getUsername()
+                + ", has been deleted from the database";
     }
 
     /**
      * A method that deletes all the users existing in the database.
      */
-    public void deleteAllUsers(){
+    public void deleteAllUsers() {
         usersRepo.deleteAll();
     }
 
     /**
      * A method that deletes all the Students existing in the database.
      */
-    public void deleteAllStudents(){
+    public void deleteAllStudents() {
         List<Users> usersList = usersRepo.findAll();
 
-        for(Users user : usersList){
-            if(user.getRole().equals("student")){
+        for (Users user : usersList) {
+            if (user.getRole().equals("student")) {
                 usersRepo.delete(user);
             }
         }
     }
+
     /**
      * A method that deletes all the Moderators existing in the database.
      */
-    public void deleteAllModerators(){
+    public void deleteAllModerators() {
         List<Users> usersList = usersRepo.findAll();
 
-        for(Users user : usersList){
-            if(user.getRole().equals("moderator")){
+        for (Users user : usersList) {
+            if (user.getRole().equals("moderator")) {
                 usersRepo.delete(user);
             }
         }
     }
+
     /**
      * A method that deletes all the Moderators existing in the database.
      */
-    public void deleteAllLecturers(){
+    public void deleteAllLecturers() {
         List<Users> usersList = usersRepo.findAll();
 
-        for(Users user : usersList){
-            if(user.getRole().equals("lecturer")){
+        for (Users user : usersList) {
+            if (user.getRole().equals("lecturer")) {
                 usersRepo.delete(user);
             }
         }
