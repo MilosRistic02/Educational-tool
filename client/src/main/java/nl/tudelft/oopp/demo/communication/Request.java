843b78f31efae4402d8b1827d1d0544355802d5f
Jos Sloof <A.J.G.sloof@student.tudelft.nl>
2021-03-09 20:35:38 +0100

    fix client/request checkstyle

--- client/src/main/java/nl/tudelft/oopp/demo/communication/Request.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/Request.java
@@ -4,17 +4,24 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
-import nl.tudelft.oopp.demo.entities.LectureRoom;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import nl.tudelft.oopp.demo.entities.LectureRoom;
 
 public class Request {
 
     private static HttpClient client = HttpClient.newBuilder().build();
 
+
+    /**
+     * Handles GET requests from the client.
+     *
+     * @param url that which the request is made
+     * @return the string response body
+     */
     public static String get(String url) {
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         HttpResponse<String> response = null;
@@ -31,6 +38,13 @@ public class Request {
         return response.body();
     }
 
+    /**
+     * Handles POST requests from the client.
+     * @param url that which the request is made
+     * @param t Json version of the object handled
+     * @return response body
+     */
+
     public static <T> String post(String url, T t) {
         Entity<T> requestBody = Entity.entity(t, MediaType.APPLICATION_JSON);
         GenericType<String> responseBodyType = new GenericType<String>(){};
@@ -44,6 +58,13 @@ public class Request {
         return responseBody;
     }
 
+    /**
+     * Handles PUT requests from the client.
+     * @param url that which the request is made
+     * @param t Json version of the object handled
+     * @return response body
+     */
+
     public static <T> String put(String url, T t) {
         Entity<T> requestBody = Entity.entity(t, MediaType.APPLICATION_JSON);
         GenericType<String> responseBodyType = new GenericType<String>(){};
@@ -56,6 +77,13 @@ public class Request {
 
         return responseBody;
     }
+
+    /**
+     * Handles DELETE requests from the client.
+     * @param url that which the request is made
+     * @param id id of the object stored in the database
+     * @return deleted object
+     */
 
     public static String delete(String url, String id) {
         GenericType<String> responseBodyType = new GenericType<String>(){};
