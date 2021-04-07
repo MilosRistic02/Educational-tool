3d69b0739120815f25d17a82dfc649cf137e5a7b
Efe <E.Sozen@student.tudelft.nl>
2021-04-07 13:07:18 +0200

    checkstyle fix

--- client/src/test/java/nl/tudelft/oopp/demo/entities/PollTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/entities/PollTest.java
@@ -1,12 +1,16 @@
 package nl.tudelft.oopp.demo.entities;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PollTest {
 
